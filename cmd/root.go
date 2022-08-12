4b82b5d04a6defe45708e4e8ceb2209313ba560f
DeeStarks <danielonoja246@gmail.com>
2022-08-12 07:23:52 +0100

    feat: graceful shutdown

--- cmd/root.go
+++ cmd/root.go
@@ -1,10 +1,16 @@
 package cmd
 
 import (
+	"database/sql"
 	"fmt"
+	"net"
+	"os"
+	"os/signal"
+	"time"
 
 	"github.com/DeeStarks/conoid/app"
 	"github.com/DeeStarks/conoid/config"
+	port "github.com/DeeStarks/conoid/domain/ports"
 	"github.com/spf13/cobra"
 )
 
@@ -20,10 +26,58 @@ var (
 				return
 			}
 
+			// Connect to the default db
+			defaultDB, err := sql.Open("sqlite3", config.DEFAULT_DB)
+			if err != nil {
+				panic(err)
+			}
+
+			// Get the number of services that are tunnelled
+			var tunnelled int
+			rec, err := port.NewDomainPort(defaultDB).ServiceProcesses().RetrieveRunning()
+			if err != nil {
+				panic(err)
+			}
+			for _, r := range rec {
+				if r.Tunnelled {
+					tunnelled++
+				}
+			}
+
+			mltcc := 10       // Maximum localtunnel connections count
+			mltcc = mltcc * 2 // Incomming and outgoing connections
+			// Record all open connections
+			// This will be used to close all on shutdown
+			openConnsCh := make(chan net.Conn, config.MAX_CONN_COUNT+mltcc)
+
 			// Start server if no argumentss were passed or the first argument is "up"
 			if len(args) <= 0 || args[0] == "start" {
-				app.NewServer().Serve()
+				go app.NewServer(openConnsCh, defaultDB).Serve()
 			}
+
+			// We'll accept graceful shutdowns when quit via SIGINT (Ctrl+C)
+			c := make(chan os.Signal, 1)
+			signal.Notify(c, os.Interrupt)
+
+			// Block until signal is received
+			<-c
+
+			// Shutdown
+			fmt.Println("Gracefully shutting down conoid...")
+		L:
+			for {
+				select {
+				case conn := <-openConnsCh:
+					err := conn.Close()
+					if err != nil {
+						panic(err)
+					}
+				// Wait for all connections to close
+				case <-time.After(time.Second * 5):
+					break L
+				}
+			}
+			fmt.Println("Done!")
 		},
 	}
 
